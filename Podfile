53eb887dd879e8ae97eb7647167f9736a9a24910
Willy <willy12262001@hotmail.com>
2017-09-13 11:48:34 +0800

    1.新增登入系統 (fb google 自創登入) 2.導入firebase 3.使帳號導入至firebase

---
+++ Podfile
@@ -0,0 +1,19 @@
+project 'Project.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Project' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Project
+  pod 'GoogleSignIn'
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+  pod 'Firebase/Storage'
+  pod 'FBSDKCoreKit'
+  pod 'FBSDKLoginKit'
+  
+end
---
+++ Project/AlertSetting.swift
@@ -0,0 +1,55 @@
+//
+//  AlertSetting.swift
+//  FirebaseTutorial
+//
+//  Created by Willy on 2017/9/11.
+//  Copyright © 2017年 AppCoda. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class AlertSetting: UIViewController {
+    
+    func setting(target:UIViewController,title:String,message:String?,BTNtitle:String) {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let defaultAction = UIAlertAction(title: BTNtitle, style: .cancel, handler: nil)
+        alertController.addAction(defaultAction)
+        
+        target.present(alertController, animated: true, completion: nil)
+    }
+    
+    func settingWithAct(target:UIViewController,title:String,message:String?,BTNtitle:String,user:User){
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: BTNtitle, style: .default) { _ in
+            user.sendEmailVerification(completion: nil)
+        }
+        let cancelAction = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(okAction)
+        
+        target.present(alertController, animated: true, completion: nil)
+    }
+    
+    func settingWithAct2(target:UIViewController,title:String,message:String?,BTNtitle:String){
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: BTNtitle, style: .default) { _ in
+            
+            guard let vc = target.storyboard?.instantiateViewController(withIdentifier: "LogInViewController") else {
+                return
+            }
+            target.present(vc, animated: true, completion: nil)
+        }
+        alertController.addAction(okAction)
+        
+        target.present(alertController, animated: true, completion: nil)
+    }
+}
+
