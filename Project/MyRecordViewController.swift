c816d418865171213ac57b9f4d396ac85c832ded
Champion <champion790713@gmail.com>
2017-10-11 12:07:55 +0800

    1.瀑布流頁面與CoreData串接

--- Project/MyRecordViewController.swift
+++ Project/MyRecordViewController.swift
@@ -13,13 +13,13 @@ class MyRecordViewController: UICollectionViewController {
     
     @IBOutlet weak var flowLayout: CollectionViewFlowLayout!
     
-    var goodlist = [Info]()
+    var goodlist = [Annotation]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        makeGoodArray()
+        goodlist = usersDataManager.runItem?.annotations?.allObjects as! [Annotation]
+//        makeGoodArray()
         print(goodlist.count)
         flowLayout.goodlist = goodlist
         
@@ -30,23 +30,23 @@ class MyRecordViewController: UICollectionViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func makeGoodArray(){
-        guard let totals = usersDataManager.userItem?.info?.count else {
-            return
-        }
-        guard totals != 0 else {
-            return
-        }
-        let total = totals - 1
-        for num in 0...total{
-            if let items = usersDataManager.userItem?.info?.allObjects {
-                
-                let item = items[num] as! Info
-                
-                goodlist.append(item)
-            }
-        }
-    }
+//    func makeGoodArray(){
+//        guard let totals = usersDataManager.runItem?.annotations?.allObjects else {
+//            return
+//        }
+//        guard totals != 0 else {
+//            return
+//        }
+//        let total = totals - 1
+//        for num in 0...total{
+//            if let items = usersDataManager.userItem?.info?.allObjects {
+//
+//                let item = items[num] as! Info
+//
+//                goodlist.append(item)
+//            }
+//        }
+//    }
 
     
     
@@ -61,11 +61,11 @@ class MyRecordViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ThumbNailCollectionViewCell
         
-        if let image = goodlist[indexPath.row].image {
+        if let image = goodlist[indexPath.row].imageData {
             cell.imageView.image = UIImage(data: image as Data)
         }
         
-        cell.caption.text = goodlist[indexPath.row].content
+        cell.caption.text = goodlist[indexPath.row].text
         cell.layer.cornerRadius = 4.0
         cell.clipsToBounds = true
         
@@ -74,7 +74,7 @@ class MyRecordViewController: UICollectionViewController {
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let vc2 = storyboard?.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-        vc2.info = goodlist[indexPath.row]
+        vc2.annotation = goodlist[indexPath.row]
         navigationController?.pushViewController(vc2, animated: true)
     }
 }
